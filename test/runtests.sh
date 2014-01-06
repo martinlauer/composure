a609e7f082c3f381ef26eefdb54ab70ed06f6e5d
Erich Smith <heartquest@gmail.com>
2014-01-05 20:55:48 -0500

    Additional debugging for Travis

--- test/runtests.sh
+++ test/runtests.sh
@@ -5,10 +5,15 @@ KSH=$(which ksh 2>/dev/null)
 ZSH=$(which zsh 2>/dev/null)
 BASH=$(which bash 2>/dev/null)
 
+echo "got KSH: $KSH"
+echo "got ZSH: $ZSH"
+echo "got BASH: $BASH"
+
 # run wvtests against available shells
 typeset test shell
 for test in t/*.sh; do
   for shell in $BASH $KSH $ZSH; do
+    echo TESTFILE="$test" SHELL=$shell $shell $test
     TESTFILE="$test" SHELL=$shell $shell $test
     [ $? -ne 0 ] && exit $?
   done
